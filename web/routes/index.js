018f745f856d265d5f5ebe3a54fa6a96f7822c24
ynouri <ynouri@users.noreply.github.com>
2018-02-09 22:51:11 -0500

    Express MVC structure

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'AHAHA INDEED' });
+});
+
+/* GET txs page. */
+router.get('/tx/:id', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
