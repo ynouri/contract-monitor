4d1fcfa19ee78d28d5b5100f327e7377a77fd630
ynouri <ynouri@users.noreply.github.com>
2018-02-16 22:55:04 -0500

    Select worker or web

--- tag_and_push.sh
+++ tag_and_push.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
-docker tag contract-monitor_web:latest nouri/contract-monitor_web:$1
-docker tag contract-monitor_worker:latest nouri/contract-monitor_worker:$1
 
-docker push nouri/contract-monitor_web:$1
-docker push nouri/contract-monitor_worker:$1
+tag_and_push_web() {
+  docker tag contract-monitor_web:latest nouri/contract-monitor_web:$1
+  docker push nouri/contract-monitor_web:$1
+}
+
+tag_and_push_worker() {
+  docker tag contract-monitor_worker:latest nouri/contract-monitor_worker:$1
+  docker push nouri/contract-monitor_worker:$1
+}
+
+
+if [ "$1" = "web" ]
+then
+  tag_and_push_web $2
+elif [ "$1" = "worker" ]
+then
+  tag_and_push_worker $2
+elif [ "$1" = "all" ]
+then
+  tag_and_push_web $2
+  tag_and_push_worker $2
+fi
